a8c702aed35a6f8e84819b2d87b45cc82bf04062
Soumya Munshi <129608507+SOUMYAMUNSHI@users.noreply.github.com>
2024-04-12 12:53:00 +0530

    Add files via upload

---
+++ Script/Alert.js
@@ -0,0 +1,20 @@
+function Alert_wrong_email()
+{
+    document.getElementById("Email__wrong-alert").innerHTML = "*Wrong Email Address";   //For wrong email address alert--Login-page.html, sign-up.html
+}
+
+
+
+
+function Alert_wrong_password()
+{
+    document.getElementById("Password__wrong-alert").innerHTML = "*Wrong Password";   //For wrong password alert--Login__page-password.html
+}
+
+
+
+
+function Alert_mismatch_password()
+{
+    document.getElementById("Password__mismatch-alert").innerHTML = "*Password mismatch";   //For mismatch password alert--Sign__up-password.html
+}
---
+++ Script/Tooltip.js
@@ -0,0 +1,35 @@
+tippy('#logout', {
+    content: 'Log out',
+    placement: 'top',
+  });
+
+  tippy('#password__reset', {
+    content: 'Change your password',
+    placement: 'top',
+  });
+
+  tippy('#password__forgot', {
+    content: 'Forgot password',
+    placement: 'top',
+  });
+
+  tippy('#Account__page', {
+    content: 'Account',
+    placement: 'bottom',
+  });
+
+  tippy('#Dev1', {
+    content: 'Developer',
+    placement: 'bottom',
+  });
+
+  tippy('#Dev2', {
+    content: 'Developer',
+    placement: 'bottom',
+  });
+
+  tippy('#Dev3', {
+    content: 'Developer',
+    placement: 'bottom',
+  });
+
